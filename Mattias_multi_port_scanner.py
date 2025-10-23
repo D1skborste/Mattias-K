3bc43162286ff93e9598d4c95a27299029dba560
D1skborste <kossan_90@hotmail.com>
2025-10-23 13:19:23 +0200

    Uppdaterat felskrivningar och placering
    Hade tid nu till att felsöka koden och göra den mer användarvänlig. Man kan mata in IP eller domain name och/eller portar som både input eller CLI

--- Mattias_multi_port_scanner.py
+++ Mattias_multi_port_scanner.py
@@ -13,28 +13,10 @@ from datetime import datetime
 
 open_ports= []
 
-#optional CLI arguments
-if len(sys.argv) == 4:
-    start_port = int(sys.argv[3])
-    max_port = int(sys.argv[4])
-    print(sys.argv)
-    
-
-elif len(sys.argv) == 2:
-    print(sys.argv)
-    # translate hostname to IPv4
-    target = socket.gethostbyname(sys.argv[1])
-
-else:
-    target = input('Enter target ip: ')
-    start_port = int(input('starting port: '))
-    max_ports = int(input('ending port: '))
 
 
-"""Kan kanske ha koden ovan som en optional CLI argument, 
-eller input om man inte gett nåot"""
 
-"""Placeringen i koden behöver justeras"""
+
 
 def start_scan(target, start_port, max_port):
    #Define the target
@@ -59,5 +41,31 @@ def start_scan(target, start_port, max_port):
         print("Could not connect to server")
         sys.exit()
 
+
+#optional CLI arguments
+
 if __name__ == "__main__":
-    start_scan("45.33.32.156", 20, 25) 
+    if len(sys.argv) == 4:
+        target = socket.gethostbyname(sys.argv[1])
+        start_port = int(sys.argv[2])
+        max_port = int(sys.argv[3])
+
+
+    elif len(sys.argv) == 2:
+    # translate hostname to IPv4
+        target = socket.gethostbyname(sys.argv[1])
+        start_port = int(input('starting port: '))
+        max_port = int(input('ending port: '))
+
+        
+    else:
+        domain_name = input('Enter target ip or domain: ')
+        target = socket.gethostbyname(domain_name)
+        start_port = int(input('starting port: '))
+        max_port = int(input('ending port: '))
+
+    
+
+    threads_count = int(input('threads count: '))
+
+    threads = []
